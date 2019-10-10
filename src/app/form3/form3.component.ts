83f77f5040e3726ac5373cfb2f52bb70e94fd923
ulestrange <lestrange.una@itsligo.ie>
2019-10-10 12:52:27 +0100

    added code to swap interest values

--- src/app/form3/form3.component.ts
+++ src/app/form3/form3.component.ts
@@ -43,16 +43,49 @@ export class Form3Component implements OnInit, OnChanges {
   ngOnChanges() {
     console.log('On Changes:', this.person);
     if (this.form3) {
-      
-      
-      this.form3.setValue({ firstName : this.person.firstName, lastName: this.person.lastName,
-      interests : this.fb.array(this.person.interests) });
-      }
+      this.form3.patchValue({
+        firstName: this.person.firstName, lastName: this.person.lastName
+      });
+
+      this.swapInterestValues(this.person.interests);
+    }
   }
 
   onSubmit() {
     console.log(this.form3.value);
 
     this.personSubmitted.emit(this.form3.value);
+  }
+
+
+  private swapInterestValues(inputInterestArray) {
+    if (inputInterestArray.length === this.interests.length) {
+      // same length - just swap values
+      this.interests.patchValue(inputInterestArray);
+    } else if (inputInterestArray.length < this.interests.length) {
+      // less interests comming in then are there already
+      this.interests.patchValue(inputInterestArray);
+      const length = this.interests.length;
+      for (let i = length; i >= inputInterestArray.length; i--) {
+        this.interests.removeAt(i);
+        console.log(inputInterestArray);
+      }
+    } else {
+      // more interests comming in that are there already
+      const length = this.interests.length;
+      const newlength = inputInterestArray.length;
+      const firstSetofValues = inputInterestArray.slice(0, length)
+      const extraInterests = inputInterestArray.slice(length, newlength);
+      // replace the existing interests
+      this.interests.patchValue(firstSetofValues);
+
+
+      // add the new interests
+      extraInterests.forEach((interest) => { this.interests.push(this.fb.control(interest)); });
+
+      console.log(inputInterestArray);
+
+    }
+
   }
 }

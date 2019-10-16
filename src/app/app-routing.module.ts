22832d4b33ea6bc36a276e6f2ce1d367bdab2536
ulestrange <lestrange.una@itsligo.ie>
2019-10-16 14:39:01 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { Form1Component} from './form1/form1.component';
+
+import { ListPeopleComponent} from './list-people/list-people.component';
 
 
+const routes: Routes = [
+  {path: 'form1', component: Form1Component},
+  {path: 'ListPeople', component: ListPeopleComponent}
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
